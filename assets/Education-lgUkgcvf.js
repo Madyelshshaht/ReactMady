b249370e8f3a9093988004d1d886e56e979e62c9
Mady <madylabtop@gmail.com>
2025-04-24 01:39:12 +0200

    Updates

---
+++ assets/Education-lgUkgcvf.js
@@ -0,0 +1,11 @@
+import{c as t,j as e}from"./index-AY-EMIZ4.js";import{T as n,a,t as i}from"./TimeLine-Bp7_TLAQ.js";/**
+ * @license lucide-react v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z",key:"j76jl0"}],["path",{d:"M22 10v6",key:"1lu8f3"}],["path",{d:"M6 12.5V16a6 3 0 0 0 12 0v-3.5",key:"1r8lef"}]],o=t("GraduationCap",s),c=()=>e.jsxs("div",{children:[e.jsxs("h2",{className:"text-2xl font-bold mb-4 flex items-center gap-2",style:{color:"#00e650"},children:[e.jsx("span",{children:e.jsx(o,{size:30})}),"Education"]}),e.jsxs(n,{className:"items-start ",sx:{[`& .${i.root}:before`]:{flex:0,padding:0}},children:[e.jsx(a,{title:"Mansoura Unversity [ FCIS ]",variant:"outlined",color:"success",location:"Mansoura, Egypt",date:" 2022 - 2026",desc:`Student in level 3 in in section\r
+                            IT and computer science including programming, algorithms, data\r
+                            structures, and web development. gained practical skills through internships,\r
+                            and collaborative projects. Proficient in multiple programming languages [\r
+                            Python, C# ] and experienced with real-world IT systems and problem\r
+                            solving.`}),e.jsx(a,{title:"High School",color:"success",location:"Shirbin, Egypt",date:"2019 - 2021",desc:"I'm finished  (General Certificate of Secondary Education) with a very good degree (90%)"})]})]});export{c as default};
---
+++ assets/Experiences-CzxUAFyU.js
@@ -0,0 +1,8 @@
+import{c as a,j as e}from"./index-AY-EMIZ4.js";import{T as t,a as s,t as n}from"./TimeLine-Bp7_TLAQ.js";/**
+ * @license lucide-react v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"M12 7v14",key:"1akyts"}],["path",{d:"M16 12h2",key:"7q9ll5"}],["path",{d:"M16 8h2",key:"msurwy"}],["path",{d:"M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z",key:"ruj8y"}],["path",{d:"M6 12h2",key:"32wvfc"}],["path",{d:"M6 8h2",key:"30oboj"}]],o=a("BookOpenText",i),c=()=>e.jsxs("div",{children:[e.jsxs("h2",{className:"text-2xl font-bold mb-4 flex items-center gap-2",style:{color:"#00e650"},children:[e.jsx("span",{children:e.jsx(o,{size:30})}),"Experiences"]}),e.jsx(t,{className:"items-start",sx:{[`& .${n.root}:before`]:{flex:0,padding:0}},children:e.jsx(s,{title:"Internship at [ Azrak Company ]",color:"success",location:"Mansoura, Egypt",date:"2024 from 5 / Nov  to 1 / October",desc:`The internship allow to refine my problem-solving skills and work within a\r
+                    dynamic team environment. Overall, my experience at Azzrk was invaluable in\r
+                    building both my technical and collaborative abilities`})})]});export{c as default};
